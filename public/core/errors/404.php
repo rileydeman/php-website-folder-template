34f3409f076b48cc938bc266002283e568ad5c25
Riley de Man <rileydeman@gmail.com>
2024-04-16 16:00:57 +0200

    Missing files
    Missing files:
    - .htaccess
    - public/core/errors/404/php

--- public/core/errors/404.php
+++ public/core/errors/404.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HOME | siteName</title>
+    <title>404 not found | siteName</title>
 
     <link rel="icon" href="">
 
@@ -17,15 +17,15 @@
 <body>
 
 <header>
-    <?php include "public/core/header.php" ?>
+    <?php include "../header.php" ?>
 </header>
 
 <main>
-    <p>Main</p>
+    <p>404 Main</p>
 </main>
 
 <footer>
-    <?php include "public/core/footer.php" ?>
+    <?php include "../footer.php" ?>
 </footer>
 
 </body>
